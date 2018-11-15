f13a579b4c9923fed626ba4b04a16dd2ab4589a2
big-suan <393954960@qq.com>
2018-11-15 12:39:41 +0800

    订单接口增加车商号字段

--- wintop-carauction-service/src/main/java/com/wintop/ms/carauction/controller/CarOrderBossApi.java
+++ wintop-carauction-service/src/main/java/com/wintop/ms/carauction/controller/CarOrderBossApi.java
@@ -159,6 +159,7 @@ public class CarOrderBossApi {
                 map.put("userName",carOrder.getUserName());
                 map.put("mobile",carOrder.getMobile());
                 map.put("auctionName",carOrder.getAuctionName());
+                map.put("userNum",carOrder.getUserNum());
                 list.add(map);
             }
             ListEntity<Map<String,Object>> listEntity = new ListEntity<>();
