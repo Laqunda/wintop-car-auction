e63fb6465a7456e4e74040301bcd1ed96512c9af
ligongzhaoyang <ligongzhaoyang>
2018-12-25 15:39:24 +0800

    增加注册中心eureka

--- wintop-carauction-service/src/main/java/com/wintop/ms/carauction/starter/CarauctionMSApplication.java
+++ wintop-carauction-service/src/main/java/com/wintop/ms/carauction/starter/CarauctionMSApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -16,11 +18,11 @@ import org.springframework.context.annotation.ComponentScan;
  * @date 2018-10-19
  *
  */
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.wintop.ms.carauction.mapper.**")
 @ComponentScan("com.wintop.ms.carauction")
-//@EnableCaching
+@EnableCaching
 public class CarauctionMSApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarauctionMSApplication.class, args);
