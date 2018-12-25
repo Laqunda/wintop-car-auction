7df6a47fb2003ba5c6ccdebb44098f48dee053be
ligongzhaoyang <ligongzhaoyang>
2018-12-25 16:19:12 +0800

    开启eureka

--- wintop-carauction-boss-api/src/main/java/com/wintop/ms/carauction/starter/BossMSApplication.java
+++ wintop-carauction-boss-api/src/main/java/com/wintop/ms/carauction/starter/BossMSApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -20,7 +20,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2018-10-19
  *
  */
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @ComponentScan("com.wintop.ms.carauction")
 public class BossMSApplication {
@@ -30,7 +30,7 @@ public class BossMSApplication {
 	}
 
 	@Bean
-	//@LoadBalanced
+	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
