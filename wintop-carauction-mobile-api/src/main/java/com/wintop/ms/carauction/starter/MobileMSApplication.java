d8722f5a7d6adc4d5870e736050d0914fbc4ee4d
ligongzhaoyang <ligongzhaoyang>
2018-12-28 15:56:10 +0800

    增加注册中心

--- wintop-carauction-mobile-api/src/main/java/com/wintop/ms/carauction/starter/MobileMSApplication.java
+++ wintop-carauction-mobile-api/src/main/java/com/wintop/ms/carauction/starter/MobileMSApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
@@ -20,7 +21,7 @@ import org.springframework.web.client.RestTemplate;
  * @date 2018-10-19
  *
  */
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @ComponentScan("com.wintop.ms.carauction")
 public class MobileMSApplication {
